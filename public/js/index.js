38b84e908abdd7be81684dfdd8d3df30f36de74c
Harold Franklyn frey <hfrankly@at-d4.21-school.ru>
2020-03-11 19:41:12 +0300

    reg form info

--- public/js/index.js
+++ public/js/index.js
@@ -1,44 +1,82 @@
 $(".register_link").click(function(){
-    $("#toggle_main").animate({position: 'absolute', left: '-=' + window.innerWidth + 'px'},500);
-    $("#toggle_register").animate({position: 'absolute', left: '-=' + window.innerWidth + 'px'},500);
+    // setTimeout(() => {$("#toggle_main").css({'display': 'none'})}, 500);
+    // $("#toggle_register").css({'display': 'block'});
+    $("#toggle_main").animate({left: '-=' + window.innerWidth + 'px'},500);
+    $("#toggle_register").animate({left: '-=' + window.innerWidth + 'px'},500);
 });
 
 $(".login_link").click(function(){
-    $("#toggle_main").animate({position: 'absolute', left: '+=' + window.innerWidth + 'px'},500);
-    $("#toggle_register").animate({position: 'absolute', left: '+=' + window.innerWidth + 'px'},500);
+    // setTimeout(() => {$("#toggle_register").css({'display': 'none'})}, 500);
+    // $("#toggle_main").css({'display': 'block'});
+    $("#toggle_main").animate({left: '+=' + window.innerWidth + 'px'},500);
+    $("#toggle_register").animate({left: '+=' + window.innerWidth + 'px'},500);
 });
 
 function validLogin(login) {
-    if (login.length < 5)
-        return ("Too short login. It must contain more 5 letters.");
-    else if (login.length > 30)
-        return ("Too big login. It must contain less 30 letters.");
+    let requiredContent = /^[\w]{8,32}$/;   // check if pass exists more than 8 characters
+    if (login.match(requiredContent) === null)
+        return ("Your login should contain not less 8 symbols.");
+    else
+        return ("Ok");
 }
 
 function validPass(pass) {
-    let requiredContent = /^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$/;   // check if pass exists at least 1 letter and
+    let requiredContent = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,32}$/;   // check if pass exists at least 1 letter and
                                                                         // 1 number and more than 8 characters
-
-    if (pass.match(requiredContent) === false)
-        return ("Your password should contain at least 1 number, 1 letter and summary 8 symbols.");
-
+    if (pass.match(requiredContent) === null)
+        return ("Your password should contain at least 1 number, 1 letter and not less 8 symbols.");
+    else
+        return ("Ok");
 }
 
 function validEmail(email) {
+    let requiredContent = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+    if (email.match(requiredContent) === null)
+        return ("It isn't email format.");
+    else
+        return ("Ok");
 }
 
-$("#register_input_login").on('keyup', () => {
-    validLogin($("#register_input_login")[0].value);
+$("#register_input_login").focusout(() => {
+    let regPop = $(".register_popup");
+    let regInp = $("#register_input_login");
+
+    if (regInp[0].value === "")
+        return;
+    regPop[0].innerHTML = validLogin(regInp[0].value);
+    regPop[0].style = 'top: ' + (regInp[0].offsetTop + parseInt(regInp.css('height')) / 2 - parseInt(regPop.css('height')) / 2) + 'px;';
+    if (regPop[0].innerHTML === 'Ok')
+        regInp[0].style = 'border-color: green';
+    else
+        regInp[0].style = 'border-color: red';
 });
 
-$("#register_input_pass").on('keyup', () => {
-    validPass($("#register_input_pass")[0].value);
+$("#register_input_pass").focusout(() => {
+    let regPop = $(".register_popup");
+    let regInp = $("#register_input_pass");
+
+    if (regInp[0].value === "")
+        return;
+    regPop[0].innerHTML = validPass(regInp[0].value);
+    regPop[0].style = 'top: ' + (regInp[0].offsetTop + parseInt(regInp.css('height')) / 2 - parseInt(regPop.css('height')) / 2) + 'px;';
+    if (regPop[0].innerHTML === 'Ok')
+        regInp[0].style = 'border-color: green';
+    else
+        regInp[0].style = 'border-color: red';
 });
 
-$("#register_input_email").on('keyup', () => {
-    validEmail($("#register_input_email")[0].value);
+$("#register_input_email").focusout(() => {
+    let regPop = $(".register_popup");
+    let regInp = $("#register_input_email");
+
+    if (regInp[0].value === "")
+        return;
+    regPop[0].innerHTML = validEmail(regInp[0].value);
+    regPop[0].style = 'top: ' + (regInp[0].offsetTop + parseInt(regInp.css('height')) / 2 - parseInt(regPop.css('height')) / 2) + 'px;';
+    if (regPop[0].innerHTML === 'Ok')
+        regInp[0].style = 'border-color: green';
+    else
+        regInp[0].style = 'border-color: red';
 });
 
-
-// ([\d][a-zA-Z])|([a-zA-Z][\d])
